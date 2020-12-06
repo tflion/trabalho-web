770713365be84077f9effd92fddbcad8d7e9bbcd
gabrielandrade709 <gabrielandrade709.ga@gmail.com>
2020-12-06 14:24:09 -0300

    🌟-conexao-bd-com-awardspace

--- conexao.php
+++ conexao.php
@@ -1,15 +1,16 @@
 <?php
-  $servidor = "localhost";
-  $usuario = "root";
-  $senha = "";
+  $servidor = "fdb25.awardspace.net";
+  $usuario = "2988268_dbclinicaveterinaria";
+  $senha = "y[6#;bDX9{:xrlBY";
+  $banco = "2988268_dbclinicaveterinaria";
 
   try {
-    $conn = new PDO("mysql:host=$servidor;dbname=dbclinicaveterinaria", $usuario, $senha);
+    $conn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
     $conn -> exec("set names utf-8");
-    // echo "Conexão Ok!";
   } catch(PDOException $e) {
     echo "Erro do PDO ".$e -> getMessage();
   } catch(Exception $e) {
     echo "Erro ".$e -> getMessage();
   }
+  
 ?>
